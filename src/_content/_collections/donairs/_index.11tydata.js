68e06cd7233a789a17d87a937b2a3dc17e3d80be
Tristan Hampton <tristanhampton@icloud.com>
2024-09-05 15:48:18 -0600

    Adding content from portfolio

---
+++ src/_content/_collections/donairs/_index.11tydata.js
@@ -0,0 +1,26 @@
+module.exports = function () {
+  return {
+    layout: 'default',
+    permalink: false,
+    eleventyComputed: {
+      donairs: data => {
+        let symbol = '🌯';
+        let rating = data.rating;
+        let donairRating = '';
+
+        if (rating == 0) {
+          return 'no 🌯\'s awarded';
+        }
+
+        for(i=1; i <= rating; i++) {
+          donairRating += symbol;
+        }
+
+        return donairRating;
+      },
+      halfRating: data => {
+        return data.rating % 1 !== 0;
+      }
+    }
+  }
+}
