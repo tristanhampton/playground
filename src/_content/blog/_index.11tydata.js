842825c6657ea7b747d50ed822664835229f6ffd
Tristan Hampton <tristanhampton@icloud.com>
2024-09-15 12:39:04 -0600

    Various fixes

--- src/_content/blog/_index.11tydata.js
+++ src/_content/blog/_index.11tydata.js
@@ -1,21 +1,5 @@
 module.exports = function () {
   return {
     layout: 'blog',
-    ogType: 'website',
-    tags: 'blog',
-    eleventyComputed: {
-      slug: data => {
-        return  data.title.toLowerCase().replaceAll(' ', '-');
-      },
-      permalink: data => {
-        return `blog/${ data.slug }/`;
-      },
-      ogTitle: data => {
-        return data.title;
-      },
-      metaDescription: data => {
-        return data.summary;
-      },
-    }
   }
 }
