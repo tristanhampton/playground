b500d6fd85376c6c08b07d18f302b79ee13d5959
Tristan Hampton <tristanhampton@icloud.com>
2025-07-21 17:05:43 -0600

    disabling bookmarks

--- src/_data/pages.js
+++ src/_data/pages.js
@@ -1,30 +1,30 @@
-import directus from './directus.js';
-import { readItems } from '@directus/sdk';
-import { richText } from './components/rich-text.js';
-import { relatedContent } from './components/related-content.js';
+// import directus from './directus.js';
+// import { readItems } from '@directus/sdk';
+// import { richText } from './components/rich-text.js';
+// import { relatedContent } from './components/related-content.js';
 
-export default async () => {
-    try {
-      return await directus.request(
-        readItems('pages', {
-          fields: [
-            '*',
-            {
-              page_components: [
-                '*',
-                {
-                  item: {
-                    ...relatedContent,
-                    ...richText,
-                  }
-                }
-              ]
-            }
-          ]
-        })
-      );
-    } catch(error) {
-      console.error(error);
-      return null;
-    }
-}
+// export default async () => {
+//     try {
+//       return await directus.request(
+//         readItems('pages', {
+//           fields: [
+//             '*',
+//             {
+//               page_components: [
+//                 '*',
+//                 {
+//                   item: {
+//                     ...relatedContent,
+//                     ...richText,
+//                   }
+//                 }
+//               ]
+//             }
+//           ]
+//         })
+//       );
+//     } catch(error) {
+//       console.error(error);
+//       return null;
+//     }
+// }
